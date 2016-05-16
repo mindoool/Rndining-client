17c5a9f8026df57f8f7872d6c4859f134fa9fe59
Minseok Kim <min3482@snu.ac.kr>
2016-05-16 23:26:44 +0900

    파일 업로드를 위한 regex처리 완

--- rndining/www/js/directives/fileReader.js
+++ rndining/www/js/directives/fileReader.js
@@ -17,17 +17,32 @@ app.directive('fileReader', function () {
 
             /* DO SOMETHING WITH workbook HERE */
             var array = [];
-            var expDate = /\d{1,2}\/\d{1,2} \w{3}/;
-            var expText = /[가-힣]+/g;
+            var expText = /[가-힣& ]+|(\d+\/\d+)+/g;
 
             var sheet_name_list = workbook.SheetNames;
+            // y - 시트명, z - 셀명, v - 셀 값
             sheet_name_list.forEach(function (y) { /* iterate through sheets */
               var worksheet = workbook.Sheets[y];
+              var num = 0;
               for (z in worksheet) {
                 /* all keys that do not begin with "!" correspond to cell addresses */
                 if (z[0] === '!') continue;
                 console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
-                array.push(JSON.stringify(worksheet[z].v));
+                do {
+                  var oldString = expText.exec(worksheet[z].v);
+                  if (oldString) {
+                    console.log(oldString);
+                    console.log(oldString[0]);
+                    //셀주소를 바탕으로 날짜, 시간대 구분하기
+                    console.log(z.slice(1,2));
+                    var newString = oldString[0].replace(/ /g, '');
+                    if (newString !== '') {
+                      array.push(newString);
+                      num += 1;
+                    }
+                    console.log(num);
+                  }
+                } while (oldString);
               }
             });
             scope.fileReader = array;
