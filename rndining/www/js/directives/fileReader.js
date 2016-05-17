f96a936eedaed1e1dcec75e59706907df7777eb2
Minseok Kim <min3482@snu.ac.kr>
2016-05-17 23:09:16 +0900

    xlsx file read 구조...

--- rndining/www/js/directives/fileReader.js
+++ rndining/www/js/directives/fileReader.js
@@ -18,7 +18,7 @@ app.directive('fileReader', function () {
             /* DO SOMETHING WITH workbook HERE */
             var array = [];
             var expText = /[가-힣& ]+|(\d+\/\d+)+/g;
-
+            var menuArray = [];
             var sheet_name_list = workbook.SheetNames;
             // y - 시트명, z - 셀명, v - 셀 값
             sheet_name_list.forEach(function (y) { /* iterate through sheets */
@@ -34,10 +34,81 @@ app.directive('fileReader', function () {
                     console.log(oldString);
                     console.log(oldString[0]);
                     //셀주소를 바탕으로 날짜, 시간대 구분하기
-                    console.log(z.slice(1,2));
+                    var cellNo = Number(z.slice(1, 3));
+                    var cellStr = z.slice(0, 1);
+                    console.log(cellNo);
+                    console.log(cellStr);
                     var newString = oldString[0].replace(/ /g, '');
                     if (newString !== '') {
-                      array.push(newString);
+                      if (cellNo == 3) {
+                        var newObj = {};
+                        newObj['date'] = newString;
+                        newObj['morning'] = [];
+                        newObj['lunch'] = [];
+                        newObj['dinner'] = [];
+                        //newObj['morning'] = {};
+                        //newObj['morning']['rice'] = [];
+                        //newObj['morning']['soup'] = [];
+                        //newObj['morning']['noodle'] = [];
+                        //newObj['morning']['maindish'] = [];
+                        //newObj['morning']['sidedish'] = [];
+                        //newObj['morning']['bread'] = [];
+                        //newObj['morning']['drink'] = [];
+                        //newObj['morning']['fruit'] = [];
+                        //newObj['morning']['salad'] = [];
+                        //newObj['lunch'] = {};
+                        //newObj['lunch']['rice'] = [];
+                        //newObj['lunch']['soup'] = [];
+                        //newObj['lunch']['maindish'] = [];
+                        //newObj['lunch']['sidedish'] = [];
+                        //newObj['dinner'] = {};
+                        //newObj['dinner']['rice'] = [];
+                        //newObj['dinner']['soup'] = [];
+                        //newObj['dinner']['maindish'] = [];
+                        //newObj['dinner']['sidedish'] = [];
+                        menuArray.push(newObj);
+                      } else if (['D', 'E', 'F'].indexOf(cellStr) >= 0) {
+                        if ([5,6,7,8,9,10,11,12,17,18,19].indexOf(cellNo) >= 0) {
+                          menuArray[0]['morning'].push(newString);
+                        } else if ([25,26,27,28,29,30,35,36,37,38,39].indexOf(cellNo) >= 0) {
+                          menuArray[0]['lunch'].push(newString);
+                        } else if ([47,48,49,50,51,52,53].indexOf(cellNo) >= 0) {
+                          menuArray[0]['dinner'].push(newString);
+                        }
+                      } else if (['H', 'I', 'J'].indexOf(cellStr) >= 0) {
+                        if ([5,6,7,8,9,10,11,12,17,18,19].indexOf(cellNo) >= 0) {
+                          menuArray[1]['morning'].push(newString);
+                        } else if ([25,26,27,28,29,30,35,36,37,38,39].indexOf(cellNo) >= 0) {
+                          menuArray[1]['lunch'].push(newString);
+                        } else if ([47,48,49,50,51,52,53].indexOf(cellNo) >= 0) {
+                          menuArray[1]['dinner'].push(newString);
+                        }
+                      } else if (['L', 'M', 'N'].indexOf(cellStr) >= 0) {
+                        if ([5,6,7,8,9,10,11,12,17,18,19].indexOf(cellNo) >= 0) {
+                          menuArray[2]['morning'].push(newString);
+                        } else if ([25,26,27,28,29,30,35,36,37,38,39].indexOf(cellNo) >= 0) {
+                          menuArray[2]['lunch'].push(newString);
+                        } else if ([47,48,49,50,51,52,53].indexOf(cellNo) >= 0) {
+                          menuArray[2]['dinner'].push(newString);
+                        }
+                      } else if (['P', 'Q', 'R'].indexOf(cellStr) >= 0) {
+                        if ([5,6,7,8,9,10,11,12,17,18,19].indexOf(cellNo) >= 0) {
+                          menuArray[3]['morning'].push(newString);
+                        } else if ([25,26,27,28,29,30,35,36,37,38,39].indexOf(cellNo) >= 0) {
+                          menuArray[3]['lunch'].push(newString);
+                        } else if ([47,48,49,50,51,52,53].indexOf(cellNo) >= 0) {
+                          menuArray[3]['dinner'].push(newString);
+                        }
+                      } else if (['T', 'U', 'V'].indexOf(cellStr) >= 0) {
+                        if ([5,6,7,8,9,10,11,12,17,18,19].indexOf(cellNo) >= 0) {
+                          menuArray[4]['morning'].push(newString);
+                        } else if ([25,26,27,28,29,30,35,36,37,38,39].indexOf(cellNo) >= 0) {
+                          menuArray[4]['lunch'].push(newString);
+                        } else if ([47,48,49,50,51,52,53].indexOf(cellNo) >= 0) {
+                          menuArray[4]['dinner'].push(newString);
+                        }
+                      }
+                      //array.push(newString);
                       num += 1;
                     }
                     console.log(num);
@@ -45,7 +116,8 @@ app.directive('fileReader', function () {
                 } while (oldString);
               }
             });
-            scope.fileReader = array;
+            //scope.fileReader = array;
+            scope.fileReader = menuArray;
             scope.$parent.$digest();
           };
           reader.readAsBinaryString(f);
